0de638b7cfd7c62a59498faaa900a8f98606ee9c
raycoarana <me@raycoarana.com>
2015-03-31 00:21:05 +0200

    Fix bad encoding

--- library/src/test/java/com/raycoarana/baindo/AndroidUnitTestHelper.java
+++ library/src/test/java/com/raycoarana/baindo/AndroidUnitTestHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Rayco Araña
+ * Copyright 2015 Rayco AraÃ±a
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
